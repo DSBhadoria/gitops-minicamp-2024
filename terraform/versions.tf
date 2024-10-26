043509b6ccac7c0d0202acffbf751ee09c56c30b
Dharamveer Singh Bhadoria <dbhadoria@opentext.com>
2024-10-26 20:18:21 +0530

    Added S3 Backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,8 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket = "gitops-tf-backend-001"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "GitopsTerraformLocks-001"
+  }
+}

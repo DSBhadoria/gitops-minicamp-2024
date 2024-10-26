a70cdd7e599546da8e86ce05329c9b4c04e26b24
Dharamveer Bhadoria <15330248+DSBhadoria@users.noreply.github.com>
2024-10-26 21:10:21 +0530

    fixed S3 Bucket region

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "gitops-tf-backend-001"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "GitopsTerraformLocks-001"
   }
 }

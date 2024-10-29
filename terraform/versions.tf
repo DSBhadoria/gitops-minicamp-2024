8515c3a76fa37f8e5ce2a2635b5213ab4e7f61ca
Dharamveer Singh Bhadoria <dbhadoria@opentext.com>
2024-10-29 12:05:49 +0530

    versions.tf formatted

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "gitops-tf-backend-002"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "gitops-tf-backend-002"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "GitopsTerraformLocks-002"
   }
 }
